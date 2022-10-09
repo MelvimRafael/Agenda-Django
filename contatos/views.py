118f4841aed2febbdb3b72f0bebff37afb14c429
MelvimRafael <melvimrafaelteclas@gmail.com>
2022-10-09 15:43:02 -0300

    Adição campo de busca

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
@@ -27,3 +29,24 @@ def ver_contato(request, contato_id):
         'contato': contato
     })
 
+def busca(request):
+    termo = request.GET.get('termo')
+    if termo is None:
+        raise Http404
+    
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos, 2) 
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html',{
+        'contatos': contatos
+    })
+    
